2e662a9c395cccd7d5f049676ee608ec42b47280
Purple6yellow <vikamper@hotmail.com>
2025-09-10 12:23:41 +0200

    10 september 2025

--- core/core/static/js/eigenontwerp/contact.js
+++ core/core/static/js/eigenontwerp/contact.js
@@ -1,28 +1,12 @@
+const foto = document.getElementById("foto_contact");
+console.log("contact pagina")
 
+foto.addEventListener('click', function(){
+    if (foto.src.includes("atelier1.jpg")){
+        foto.src = "http://127.0.0.1:8000/img/atelier2.jpg";
+        //alert(foto.src)
+    }else{
+        foto.src = "http://127.0.0.1:8000/img/atelier1.jpg";
+       // alert(foto.src) }
+ }})
 
-const afbeeldingen = ["img/atelier1.jpg", "img/atelier2.jpg", "img/ateliers3.jpg"];
-//const afbeeldingen = ["{% static 'img/atelier1.jpg' %}", "{% static 'img/atelier2.jpg' %}", "{% static 'img/atelier3.jpg' %}"];
-let index = 0 ;
-//alert ("start van interval")
-
-setInterval (() =>{
-    index = (index + 1 ) % afbeeldingen.length;
-    foto.src = afbeeldingen[index];}
-,4000); // elke 4 seconden wisselen 
-
-if (foto.src.includes("img/atelier1.jpg")){
-    alert ("foto include")
-}else {
-    foto.src('img/atelier1.jpg')
-}
-
-//
-// // const foto = document.getElementById("foto_contact");
-// console.log("contact pagina")
-// foto.addEventListener('click', function(){
-//    if (foto.src.includes("atelier1.jpg")){
-//        foto.src = "img/atelier2.jpg";
-//    }else{
-//        foto.src = "img/atelier1.jpg";
-//   }
-//  })
