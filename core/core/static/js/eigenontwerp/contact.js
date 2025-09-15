b4afecfba202ab1aec77347e471e7d0968028e86
Purple6yellow <vikamper@hotmail.com>
2025-09-15 16:03:34 +0200

    15 september

--- core/core/static/js/eigenontwerp/contact.js
+++ core/core/static/js/eigenontwerp/contact.js
@@ -3,10 +3,10 @@ console.log("contact pagina")
 
 foto.addEventListener('click', function(){
     if (foto.src.includes("atelier1.jpg")){
-        foto.src = "http://127.0.0.1:8000/img/atelier2.jpg";
+        foto.src = "{% static 'img/atelier2.jpg' %}";
         //alert(foto.src)
     }else{
-        foto.src = "http://127.0.0.1:8000/img/atelier1.jpg";
+        foto.src = "{% static 'img/atelier1.jpg' %}";
        // alert(foto.src) }
  }})
 
